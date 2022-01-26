848892f87a5eaabc1bb146c6e7474690b29dd0f0
Tamirchn1@gmail.com <T2151991t>
2022-01-26 13:35:07 +0100

    inital commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="nav">
+        <Switch>
+          <Route exact path="/" component={} />
+          <Route exact path="/" component={} />
+          <Route exact path="/" component={} />
+          <Route exact path="/" component={} />
+          <Route exact path="/" component={} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+function Nav() {
+  return (
+  <div id="hamburger-icon" onclick="toggleMobileMenu(this)">
+  <div class="bar1"></div>
+  <div class="bar2"></div>
+  <div class="bar3"></div>
+     </div>;
+  );
+}
+export default Nav;
